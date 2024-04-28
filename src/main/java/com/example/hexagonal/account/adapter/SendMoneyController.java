3415e73c2b140170e54fd1ac77ce599c31262107
jeongyeongjae <yeonjaery@gmail.com>
2024-04-28 20:01:24 +0900

    add: 웹 어댑터 구현하기

---
+++ src/main/java/com/example/hexagonal/account/adapter/SendMoneyController.java
@@ -0,0 +1,33 @@
+package com.example.hexagonal.account.adapter;
+
+import com.example.hexagonal.account.application.port.in.SendMoneyCommand;
+import com.example.hexagonal.account.application.port.in.SendMoneyUseCase;
+import com.example.hexagonal.account.domain.Account;
+import com.example.hexagonal.account.domain.Money;
+import com.example.hexagonal.common.WebAdapter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@WebAdapter
+@RestController
+@RequiredArgsConstructor
+public class SendMoneyController {
+
+    private final SendMoneyUseCase sendMoneyUseCase;
+
+    @PostMapping(path = "/accounts/send/{sourceAccountId}/{targetAccountId}/{amount}")
+    void sendMoney(
+            @PathVariable("sourceAccountId") Long sourceAccountId,
+            @PathVariable("targetAccountId") Long targetAccountId,
+            @PathVariable("amount") Long amount) {
+
+        SendMoneyCommand command = new SendMoneyCommand(
+                new Account.AccountId(sourceAccountId),
+                new Account.AccountId(targetAccountId),
+                Money.of(amount));
+
+        sendMoneyUseCase.sendMoney(command);
+    }
+}
